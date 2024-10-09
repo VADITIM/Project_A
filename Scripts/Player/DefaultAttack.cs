d2cacfc5b938f2ba81a7266b14aae3c19f62c940
Vadim Niedental <vadim.niedental@gmail.com>
2024-10-09 20:17:01 +0200

    Added a Hitbox to the Attack. Interacting with Enviornment and Enemies.

--- Scripts/Player/DefaultAttack.cs
+++ Scripts/Player/DefaultAttack.cs
@@ -1,5 +1,4 @@
 using Godot;
-using System;
 
 public partial class DefaultAttack : Node
 {
@@ -12,15 +11,20 @@ public partial class DefaultAttack : Node
     private Vector2 attackDirection;
 
     private CharacterBody2D player;
-
     private AnimatedSprite2D playerSprite;
     private AnimatedSprite2D effectSprite;
+    private Area2D hitbox; 
 
-    public DefaultAttack(CharacterBody2D player, AnimatedSprite2D playerSprite, AnimatedSprite2D effectSprite)
+    public DefaultAttack(CharacterBody2D player, AnimatedSprite2D playerSprite, AnimatedSprite2D effectSprite, Area2D hitbox)
     {
         this.player = player;
         this.playerSprite = playerSprite;
         this.effectSprite = effectSprite;
+        this.hitbox = hitbox;
+
+        hitbox.AreaEntered += OnAreaEntered;
+
+        hitbox.Monitoring = false;
     }
 
     public void UpdateAttack(float delta)
@@ -38,6 +42,8 @@ public partial class DefaultAttack : Node
                 windup = false;
                 attackCooldownTimer = cooldown;
                 effectSprite.Visible = false;
+
+                hitbox.Monitoring = false;
             }
         }
         else
@@ -60,6 +66,9 @@ public partial class DefaultAttack : Node
 
         effectSprite.Visible = true;
 
+        // Activate hitbox when attack starts
+        hitbox.Monitoring = true;
+
         PlayAttackAnimations();
     }
 
@@ -80,12 +89,20 @@ public partial class DefaultAttack : Node
         effectSprite.Rotation = angleToMouse;
 
         playerSprite.Stop();
-        playerSprite.Play("default_attack"); 
+        playerSprite.Play("default_attack");
         playerSprite.Frame = 0;
 
         effectSprite.Stop();
         effectSprite.Play(effectAnimation);
         effectSprite.Frame = 0;
+    }
+
+    private void OnAreaEntered(Area2D other)
+    {
+        if (other is Area2D target)
+        {
+            GD.Print("Hit: " + target.Name);
+        }
     }
 
     public Vector2 GetAttackDirection()

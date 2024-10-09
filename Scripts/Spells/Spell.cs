672eedae0444a7f1b82b39a6cd777f858e4d1db2
Vadim Niedental <vadim.niedental@gmail.com>
2024-10-09 13:55:39 +0200

    Added variables for inheritance.

--- Scripts/Spells/Spell.cs
+++ Scripts/Spells/Spell.cs
@@ -3,22 +3,23 @@ using Godot;
 public abstract partial class Spell : Node
 {
     protected CharacterBody2D player;
+    protected bool windup = false;
+    protected float time;
     protected float cooldown;
-    protected float duration;
-    protected bool isActive;
-    protected Vector2 direction;
 
-    public Spell(CharacterBody2D player, float cooldown, float duration)
+    protected float attackCooldownTimer;
+    protected float currentAttackCooldownTimer;
+    protected Vector2 attackdirection;
+
+    public Spell(CharacterBody2D player, float cooldown, float time)
     {
         this.player = player;
         this.cooldown = cooldown;
-        this.duration = duration;
-        isActive = false;
+        this.time = time;
+        windup = false;
     }
 
     public abstract void Activate();
     public abstract void Deactivate();
     public abstract void Update(float delta);
-
-    public bool IsActive() => isActive;
 }

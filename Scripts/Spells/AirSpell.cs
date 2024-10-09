2cca952c5b7ee23d029c144d71e827b5fd3f5995
Vadim Niedental <vadim.niedental@gmail.com>
2024-10-09 20:19:32 +0200

    AirSpell is now a child of Spell.

--- Scripts/Spells/AirSpell.cs
+++ Scripts/Spells/AirSpell.cs
@@ -1,68 +1,16 @@
-using Godot;
 using System;
+using Godot;
 
-public partial class AirSpell : Node
+public partial class AirSpell : Spell
 {
-    private bool windup = false;
-    private float time = 1.55f;
-    private float cooldown = 1f;
-    
-    private float attackCooldownTimer = 0f;
-    private float currentAttackCooldownTimer = 0f;
-    private Vector2 attackDirection;
-
-    private CharacterBody2D player;
-    
-    private AnimatedSprite2D playerSprite;
-    private AnimatedSprite2D effectSprite;
-
-    public AirSpell(CharacterBody2D player, AnimatedSprite2D effectSprite)
+    public AirSpell(CharacterBody2D player, AnimatedSprite2D effectSprite) 
+        : base(player, effectSprite) 
     {
-        this.player = player;
-        this.effectSprite = effectSprite;
-        this.effectSprite.Visible = false;
+        time = 1.55f;
+        cooldown = .3f; 
     }
 
-    public void UpdateAttack(float delta)
-    {
-        AirSpellAttack(delta);
-    }
-
-    public void AirSpellAttack(float delta)
-    {
-        if (windup)
-        {
-            currentAttackCooldownTimer -= delta;
-            if (currentAttackCooldownTimer <= 0)
-            {
-                windup = false;
-                attackCooldownTimer = cooldown;
-                effectSprite.Visible = false;
-            }
-        }
-        else
-        {
-            attackCooldownTimer -= delta;
-            if (attackCooldownTimer <= 0 && Input.IsActionJustPressed("spell1"))
-            {
-                StartAttack();
-            }
-        }
-    }
-
-    private void StartAttack()
-    {
-        windup = true;
-        currentAttackCooldownTimer = time;
-        Vector2 directionToMouse = player.GetGlobalMousePosition() - player.GlobalPosition;
-        attackDirection = directionToMouse.Normalized();
-        GD.Print("Spell burning in direction: " + attackDirection);
-        effectSprite.Visible = true;
-
-        UpdateAnimation();
-    }
-
-    private void UpdateAnimation()
+    protected override void UpdateAnimation()
     {
         if (Math.Abs(attackDirection.X) > Math.Abs(attackDirection.Y))
         {
@@ -104,12 +52,6 @@ public partial class AirSpell : Node
         }
         effectSprite.Stop();
         effectSprite.Play();
-        effectSprite.Frame = 0; 
-    }
-
-
-    public Vector2 GetAttackDirection()
-    {
-        return attackDirection;
+        effectSprite.Frame = 0;
     }
 }
